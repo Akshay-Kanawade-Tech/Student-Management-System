c6969e0baa7972324f207b27a7c8551adaf7c891
Akshay-Mahagan <tech.akshayk@gmail.com>
2023-10-29 14:39:31 +0530

    Added comment in Controller layer

--- student-management-system/src/main/java/com/arktechsms/controller/StudentController.java
+++ student-management-system/src/main/java/com/arktechsms/controller/StudentController.java
@@ -1,5 +1,5 @@
 package com.arktechsms.controller;
-
+//controller layer for application
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
